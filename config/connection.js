817ffa3d0db5c4a059d6bef6fe9c2f603fc3930a
Andrew Albrecht <andrewalbrecht@utexas.edu>
2019-02-20 19:31:22 -0600

    heroku debug

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,20 @@
+// Set up MySQL connection.
 var mysql = require("mysql");
-// var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSBD_URL);
+    // DB is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "burger_db"
-  });
+    // DB is local on localhost
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 8889,
+        user: 'root',
+        password: 'root',
+        database: 'burger_db'
+    })
 };
-
+// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -21,4 +23,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+// Export connection for our ORM to use.
 module.exports = connection;
